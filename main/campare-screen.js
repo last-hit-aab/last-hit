5b3c1794f23441802bfc9ef0af03e3255e52a4ac
last-hit-b <last.hit.b@yandex.com>
2019-10-02 21:21:35 +0800

    format

--- main/campare-screen.js
+++ main/campare-screen.js
@@ -1,33 +1,28 @@
-const compareImages = require("resemblejs/compareImages");
-
+const compareImages = require('resemblejs/compareImages');
 
 const compareScreen = async (record, replay) => {
-    const options = {
-        output: {
-            errorColor: {
-                red: 255,
-                green: 0,
-                blue: 255
-            },
-            errorType: "movement",
-            transparency: 0.3,
-            largeImageThreshold: 1200,
-            useCrossOrigin: false,
-            outputDiff: true
-        },
-        scaleToSameSize: true,
-        ignore: "antialiasing"
-    };
+	const options = {
+		output: {
+			errorColor: {
+				red: 255,
+				green: 0,
+				blue: 255
+			},
+			errorType: 'movement',
+			transparency: 0.3,
+			largeImageThreshold: 1200,
+			useCrossOrigin: false,
+			outputDiff: true
+		},
+		scaleToSameSize: true,
+		ignore: 'antialiasing'
+	};
 
-    // The parameters can be Node Buffers
-    // data is the same as usual with an additional getBuffer() function
-    const data = await compareImages(
-        record,
-        replay,
-        options
-    );
+	// The parameters can be Node Buffers
+	// data is the same as usual with an additional getBuffer() function
+	const data = await compareImages(record, replay, options);
 
-    return data.getBuffer();
-}
+	return data.getBuffer();
+};
 
 module.exports = { compareScreen };
