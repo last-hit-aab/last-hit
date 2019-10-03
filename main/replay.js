bbf7c62647207be34ebb59144c2d23e9d8bc204f
last-hit-b <last.hit.b@yandex.com>
2019-10-03 13:11:07 +0800

    rename files

--- main/replay.js
+++ main/replay.js
@@ -1,6 +1,6 @@
 const { ipcMain } = require('electron');
 const { logger } = require('./logger');
-const replayer = require('../replayer');
+const replayer = require('../replayer/replay');
 
 const funcs = replayer({ emitter: ipcMain, logger });
 module.exports = funcs;
