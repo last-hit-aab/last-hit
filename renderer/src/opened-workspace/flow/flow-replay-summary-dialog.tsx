a070a024ca0edc360e5d72e4a554ac6b24c97eac
last-hit-b <last.hit.b@yandex.com>
2019-10-31 16:29:11 +0800

    #34 ide summary dialog

--- renderer/src/opened-workspace/flow/flow-replay-summary-dialog.tsx
+++ renderer/src/opened-workspace/flow/flow-replay-summary-dialog.tsx
@@ -4,9 +4,15 @@ import {
 	DialogActions,
 	DialogContent,
 	DialogTitle,
+	Grid,
+	List,
+	ListItem,
+	ListItemText,
 	makeStyles,
+	Tab,
+	Tabs,
 	TextField,
-	Grid
+	Typography
 } from '@material-ui/core';
 import React, { Fragment } from 'react';
 import { getTheme } from '../../global-settings';
@@ -14,6 +20,60 @@ import { getTheme } from '../../global-settings';
 const myTheme = getTheme();
 const useStyles = makeStyles(theme => ({
 	root: {
+		overflow: 'hidden'
+	},
+	summary: {
+		display: 'grid',
+		gridTemplateColumns: 'auto auto auto auto auto',
+		gridColumnGap: theme.spacing(2)
+	},
+	tabs: {
+		gridColumn: 'span 5',
+		minHeight: 'unset',
+		height: theme.spacing(4),
+		'& .MuiTabs-flexContainer > button': {
+			minWidth: theme.spacing(15),
+			paddingTop: 0,
+			paddingBottom: 0,
+			minHeight: 'unset',
+			height: theme.spacing(4),
+			'& > .MuiTab-wrapper': {
+				textOverflow: 'ellipsis',
+				overflowX: 'hidden',
+				whiteSpace: 'nowrap'
+			}
+		},
+		'& .MuiTabs-indicator': {
+			backgroundColor: theme.palette.primary.light
+		}
+	},
+	errorTab: {
+		gridColumn: 'span 5',
+		width: 'unset',
+		height: theme.spacing(50),
+		paddingTop: theme.spacing(1),
+		paddingBottom: theme.spacing(1),
+		'&[data-hidden=true]': {
+			display: 'none'
+		},
+		'& > *': {
+			flexGrow: 1,
+			'& > div': {
+				height: '100%',
+				alignItems: 'stretch',
+				'&:before, &:after': {
+					display: 'none'
+				}
+			}
+		}
+	},
+	screenshotTab: {
+		gridColumn: 'span 5',
+		width: 'unset',
+		height: theme.spacing(50),
+		paddingTop: theme.spacing(1),
+		paddingBottom: theme.spacing(1),
+		overflow: 'auto',
 		'&:hover::-webkit-scrollbar-thumb': {
 			opacity: 1
 		},
@@ -23,50 +83,75 @@ const useStyles = makeStyles(theme => ({
 		},
 		'&::-webkit-scrollbar-thumb': {
 			backgroundColor: myTheme.textScrollBarThumbBackgroundColor
+		},
+		'&[data-hidden=true]': {
+			display: 'none'
 		}
 	},
-	summary: {
-		display: 'grid',
-		gridTemplateColumns: 'auto auto auto auto auto',
-		gridColumnGap: theme.spacing(2)
-	},
-	error: {
-		gridColumn: 'span 2'
+	ajaxTab: {
+		gridColumn: 'span 5',
+		width: 'unset',
+		height: theme.spacing(50),
+		paddingTop: theme.spacing(1),
+		paddingBottom: theme.spacing(1),
+		overflow: 'auto',
+		'&:hover::-webkit-scrollbar-thumb': {
+			opacity: 1
+		},
+		'&::-webkit-scrollbar': {
+			backgroundColor: 'transparent',
+			width: 8
+		},
+		'&::-webkit-scrollbar-thumb': {
+			backgroundColor: myTheme.textScrollBarThumbBackgroundColor
+		},
+		'&[data-hidden=true]': {
+			display: 'none'
+		},
+		'& > ul': {
+			width: '100%',
+			'& > div': {
+				paddingTop: 0,
+				paddingBottom: 0,
+				'& > div': {
+					marginTop: 0,
+					marginBottom: 0,
+					display: 'flex',
+					'& > span': {
+						flexGrow: 1,
+						overflow: 'hidden',
+						marginRight: theme.spacing(2),
+						whiteSpace: 'nowrap',
+						textOverflow: 'ellipsis'
+					}
+				}
+			}
+		}
 	}
 }));
 
-export default (props: { data: { summary: any | null; error: string | null }; close: () => void }): JSX.Element => {
+export default (props: {
+	data: { summary: any | null; error: string | null; errorStack: string | null };
+	close: () => void;
+}): JSX.Element => {
 	const { data, close } = props;
+	const { error = null, errorStack = null, summary = null } = data || {};
 	const classes = useStyles({});
 
-	if (!data || !data.summary) {
+	const [tabIndex, setTabIndex] = React.useState(0);
+	const handleTabChange = (event: any, newValue: number): void => setTabIndex(newValue);
+
+	if (!summary) {
 		return <Fragment />;
 	}
 
-	const title = `Replay Summary, ${data.error ? 'Failed' : 'Done'}`;
-	let errorDisplay;
-	if (data.error) {
-		errorDisplay = (
-			<TextField
-				label="Error"
-				defaultValue={data.error}
-				className={classes.error}
-				margin="dense"
-				InputProps={{
-					readOnly: true
-				}}
-			/>
-		);
-	}
-
-	const summary = data.summary;
+	const title = `Replay Summary, ${error ? 'Error Occurred' : 'Successfully'}`;
 
 	return (
 		<Dialog open={true} onClose={() => close()} fullWidth={true} disableBackdropClick={true} maxWidth="lg">
 			<DialogTitle>{title}</DialogTitle>
 			<DialogContent className={classes.root}>
 				<Grid container className={classes.summary}>
-					{errorDisplay}
 					<TextField
 						label="Step Count"
 						defaultValue={summary.numberOfStep}
@@ -107,6 +192,47 @@ export default (props: { data: { summary: any | null; error: string | null }; cl
 							readOnly: true
 						}}
 					/>
+					<Tabs value={tabIndex} onChange={handleTabChange} className={classes.tabs}>
+						<Tab label="Error" key="error" />
+						<Tab label="Screenshot Comparison" key="screenshot" />
+						<Tab label="Slow Ajax" key="ajax" />
+					</Tabs>
+					<Grid item container data-hidden={tabIndex !== 0} role="tabpanel" className={classes.errorTab}>
+						<TextField
+							multiline
+							defaultValue={errorStack ? errorStack : 'Everything is OK.'}
+							margin="dense"
+							fullWidth
+							InputProps={{ readOnly: true, 'aria-label': 'naked' }}
+						/>
+					</Grid>
+					<Grid item container data-hidden={tabIndex !== 1} role="tabpanel" className={classes.screenshotTab}>
+						{(summary.screenCompareList || {}).length === 0 ? (
+							<Typography variant="body2" gutterBottom>
+								No screenshot comparison.
+							</Typography>
+						) : (
+							<div />
+						)}
+					</Grid>
+					<Grid item container data-hidden={tabIndex !== 2} role="tabpanel" className={classes.ajaxTab}>
+						{(summary.slowAjaxRequest || []).length === 0 ? (
+							<Typography variant="body2" gutterBottom>
+								No slow ajax requests.
+							</Typography>
+						) : (
+							<List dense={true}>
+								{summary.slowAjaxRequest.map((item: { url: string; time: number }, index: number) => {
+									const { url, time } = item;
+									return (
+										<ListItem key={`${url}-${index}`} button={true}>
+											<ListItemText primary={url} secondary={`${time}ms`} />
+										</ListItem>
+									);
+								})}
+							</List>
+						)}
+					</Grid>
 				</Grid>
 			</DialogContent>
 			<DialogActions>
--- replayer/lib/replay.js
+++ replayer/lib/replay.js
@@ -1203,7 +1203,15 @@ const launch = () => {
 						// failed, prepare for next step
 						// send back
 						replayer.getSummary().handleError(step, e);
-						waitForNextStep({ event, replayer, storyName, flowName, index, error: e.message });
+						waitForNextStep({
+							event,
+							replayer,
+							storyName,
+							flowName,
+							index,
+							error: e.message,
+							errorStack: e.stack
+						});
 					}
 			}
 		});
@@ -1215,6 +1223,7 @@ const launch = () => {
 		options.event.reply(`replay-step-end-${generateKeyByString(storyName, flowName)}`, {
 			index: options.index,
 			error: options.error,
+			errorStack: options.errorStack,
 			summary: replayer.getSummaryData()
 		});
 	};
@@ -1246,7 +1255,15 @@ const launch = () => {
 			replayer.getSummary().handleError((flow.steps || [])[0] || {}, e);
 			// failed, prepare for next step
 			// send back
-			waitForNextStep({ event, replayer, storyName, flowName: flow.name, index, error: e.message });
+			waitForNextStep({
+				event,
+				replayer,
+				storyName,
+				flowName: flow.name,
+				index,
+				error: e.message,
+				errorStack: e.stack
+			});
 		}
 	});
 	return handle;
