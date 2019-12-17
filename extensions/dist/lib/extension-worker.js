498917ce9a3e850c9d183c9d712f1bccaa2f0cac
last-hit-b <last.hit.b@yandex.com>
2019-12-17 14:38:25 +0800

    fixed: wrong data returns from extension worker when no workspace extension

--- extensions/dist/lib/extension-worker.js
+++ extensions/dist/lib/extension-worker.js
@@ -201,11 +201,7 @@ var ExtensionWorker = /** @class */ (function () {
             else {
                 // child process not found, generate an ignore reply
                 resolve();
-                _this.getEmitter().emit("data" /* DATA */, {
-                    type: types_1.ExtensionEventTypes.DATA_TRANSMITTED,
-                    extensionId: extensionId,
-                    data: { ignore: true }
-                });
+                _this.getEmitter().emit("data" /* DATA */, { ignore: true });
             }
         });
     };
--- extensions/lib/extension-worker.ts
+++ extensions/lib/extension-worker.ts
@@ -203,11 +203,7 @@ class ExtensionWorker implements IExtensionWorker {
 			} else {
 				// child process not found, generate an ignore reply
 				resolve();
-				this.getEmitter().emit(WorkerEvents.DATA, {
-					type: ExtensionEventTypes.DATA_TRANSMITTED,
-					extensionId,
-					data: { ignore: true }
-				} as ExtensionDataTransmittedIgnoreEvent);
+				this.getEmitter().emit(WorkerEvents.DATA, { ignore: true });
 			}
 		});
 	}
